d9bfa6e7c193bed8310e0dc5662194f982d066f2
Tanya S <tatyana.slabodchykava@gmail.com>
2025-03-02 16:08:52 +0200

    adding translation to model

--- backend/src/db/models/question.ts
+++ backend/src/db/models/question.ts
@@ -2,11 +2,13 @@ import { Schema, model } from 'mongoose'
 
 interface IQuestion {
   item: string
+  translation: string
   tags?: string[]
 }
 
 const questionSchema = new Schema<IQuestion>({
   item: { type: String, required: true },
+  translation: { type: String, required: true },
   tags: { type: [String] },
 })
 
