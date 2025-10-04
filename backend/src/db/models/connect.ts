0145cfda9b0ae5076fbc341b34a65ae773eaf8e3
Tanya S <tatyana.slabodchykava@gmail.com>
2025-10-04 20:49:27 +0300

    logs

--- backend/src/db/models/connect.ts
+++ backend/src/db/models/connect.ts
@@ -4,5 +4,7 @@ export const dbConnect = async () => {
   const uri =
     process.env.MONGODB_DB_URI ||
     'mongodb://root:root@mongo:27017/lanuage-cards?authSource=admin'
+  console.log('NODE_ENV', process.env.NODE_ENV)
+  console.log('uri ends with', uri.slice(-50))
   await connect(uri)
 }
