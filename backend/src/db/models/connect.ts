94e71bd9712b57037b6cffc876b14d02493852ea
Tanya S <tatyana.slabodchykava@gmail.com>
2025-10-04 20:28:58 +0300

    db-db uri

--- backend/src/db/models/connect.ts
+++ backend/src/db/models/connect.ts
@@ -2,7 +2,7 @@ import { connect } from 'mongoose'
 
 export const dbConnect = async () => {
   const uri =
-    process.env.MONGODB_URI ||
+    process.env.MONGODB_DB_URI ||
     'mongodb://root:root@mongo:27017/lanuage-cards?authSource=admin'
   await connect(uri)
 }
